c4f1c477521bb83c1300be61608e2be8f9b0fa3c
silviamacchi <silviaangela.macchi@mail.polimi.it>
2025-07-07 16:10:29 +0200

    same for ol-layerswitchers

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,5 @@
 //import 'ol/ol.css';
-import 'ol-layerswitcher/dist/ol-layerswitcher.css';
+//import 'ol-layerswitcher/dist/ol-layerswitcher.css';
 import { Map, View, Overlay } from 'ol';
 import { Tile, Image, Group, Vector } from 'ol/layer';
 import LayerGroup from 'ol/layer/Group';
